6c75fcdd63080e929a41dd2eb31d385239c91ea1
BNj10 <saquinbenjie7@gmail.com>
2026-09-02 14:30:01 +0800

    Add new migration for 20260902_060022 and update migration index
    - Created a new migration file 20260902_060022.ts with up and down functions.
    - Updated the migrations index to include the new migration.

--- src/migrations/index.ts
+++ src/migrations/index.ts
@@ -1,4 +1,5 @@
-import * as migration_20260831_221135_portfolio from './20260831_221135_portfolio'
+import * as migration_20260831_221135_portfolio from './20260831_221135_portfolio';
+import * as migration_20260902_060022 from './20260902_060022';
 
 export const migrations = [
   {
@@ -6,4 +7,9 @@ export const migrations = [
     down: migration_20260831_221135_portfolio.down,
     name: '20260831_221135_portfolio',
   },
-]
+  {
+    up: migration_20260902_060022.up,
+    down: migration_20260902_060022.down,
+    name: '20260902_060022'
+  },
+];
